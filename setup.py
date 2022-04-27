e275cb44f826e70b02641d667d064261e0143113
Reinis Melbardis <rmelbardis@gmail.com>
2022-04-27 16:14:03 +0100

    environment set-up

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open('requirements.txt') as f:
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='BondingCurveNexus',
-      version="1.0",
-      description="Project Description",
+      version="0.1",
+      description="Modelling Nexus Tokenomics and Bonding Curve",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
