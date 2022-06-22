c4894730a69a02bb2773e570e469480318d06c70
Reinis Melbardis <rmelbardis@gmail.com>
2022-06-22 14:36:05 +0100

    remove dictionary versions of variables

--- BondingCurveNexus/model_params.py
+++ BondingCurveNexus/model_params.py
@@ -2,37 +2,56 @@
 # Define fixed modelling parameters for simulation #
 #--------------------------------------------------#
 
+# number of days to run the model for
+model_days = 366
 
-dictionary = {
-    # number of days to run the model for
-    'model_days': 366,
+# mean number of users entering and exiting the system
+# to be modelled by poisson distribution
+lambda_entries = 10
+lambda_exits = 10
 
-    # mean number of users entering and exiting the system
-    # to be modelled by poisson distribution
-    'lambda_entries': 10,
-    'lambda_exits': 10,
+# lognormal distribution of size of ENTRIES AND EXITS in ETH
+# parameterised to median value being ~1 ETH, upper quartile ~4-5 ETH
+sale_shape = 2
+sale_loc = 0
+sale_scale = 1
 
-    # lognormal distribution of size of ENTRIES AND EXITS in ETH
-    # parameterised to median value being ~1 ETH, upper quartile ~4-5 ETH
-    'sale_shape': 2,
-    'sale_loc': 0,
-    'sale_scale': 1,
+# normal distribution of daily change in active COVER AMOUNT
+cover_amount_mean = 0.005
+cover_amount_stdev = 0.07
 
-    # normal distribution of daily change in active COVER AMOUNT
-    'cover_amount_mean': 0.005,
-    'cover_amount_stdev': 0.07,
+# lognormal distribution of daily PREMIUM INCOME
+# parameterised to have median value of a handful of ETH,
+# upper quartile around 10 ETH and the occasional multi-million $ day
+premium_shape = 2
+premium_loc = 0.02
+premium_scale = 3
 
-    # lognormal distribution of daily PREMIUM INCOME
-    # parameterised to have median value of a handful of ETH,
-    # upper quartile around 10 ETH and the occasional multi-million $ day
-    'premium_shape': 2
-    'premium_loc': 0.02
-    'premium_scale': 3
+# annual investment income %
+investment_apy = 0.02
+daily_investment_return = (1 + investment_apy) ** (1 / 365) - 1
 
-    # annual investment income %
-    'investment_apy': 0.02
+# CLAIM frequency
+claim_prob = 0.03
 
+# lognormal distribution of claim size
+# parameterised to have values of at least 1 ETH, the median value to be ~9 ETH,
+# upper quartile around 30 ETH and the occasional multi-million payout
+claim_shape = 2
+claim_loc = 1
+claim_scale = 15
 
-    # probability of exit per day of being in the exit queue at 100% book value
-    'p_exit_full': 0.1
-}
+# claim assessment reward (assuming premium was 1% of claim size)
+claim_ass_reward = 0.002
+
+# wnxm price movements (normal distribution of % change per day)
+wnxm_drift = 0
+wnxm_diffusion = 0.0655
+
+# parameters related to closing wnxm gap to book value
+# time preference of users
+wnxm_discount_to_book = 0.05
+# assumed single trade for closing the gap
+gap_eth_sale = 60
+# probability of exit per day of being in the exit queue at 100% book value
+p_exit_full = 0.1
--- BondingCurveNexus/sys_class.py
+++ BondingCurveNexus/sys_class.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from BondingCurveNexus import sys_params
 from BondingCurveNexus import model_params
 
@@ -6,16 +8,10 @@ Define the Nexus Mutual system as a class
 This allows us to track the different variables as they change over time
 '''
 
-daily_investment_return: (1+investment_apy)**(1/365) - 1
-
 class NexusSystem:
-
-    # SYSTEM PARAMETERS - CURRENT FIXED BUT MAY BE SUBJECT TO CHANGE #
-    system_vals = sys_params.dictionary
-    model_vals = model_params.dictionary
-
 
     def __init__(self):
 
         # initiate model parameters
         self.current_day = 0
+        self.
