66d9a3e7590c8e96e2db836dec0a44a441bf8130
Reinis Melbardis <rmelbardis@gmail.com>
2022-10-19 14:42:33 +0100

    upgrade 0.6 to proper one v two sided comparison

--- BondingCurveNexus/sys_params.py
+++ BondingCurveNexus/sys_params.py
@@ -5,9 +5,9 @@ import requests
 
 # NEXUSTRACKER VALUES TODAY - UPDATES REQUIRED REGULARLY #
 # TODO: pull these in automatically
-nxm_supply_now = 6_784_279
-act_cover_now = 136_697
-cap_pool_now = 153_789
+nxm_supply_now = 6_783_026
+act_cover_now = 142_282
+cap_pool_now = 153_887
 
 # coingecko price api
 price_url = 'https://api.coingecko.com/api/v3/simple/price'
