4a9ace38b022a4460d08e61a2d886b4b329edc54
Reinis Melbardis <rmelbardis@gmail.com>
2022-12-20 16:45:34 +0000

    additional run experiments

--- BondingCurveNexus/sys_params.py
+++ BondingCurveNexus/sys_params.py
@@ -5,9 +5,9 @@ import requests
 
 # NEXUSTRACKER VALUES TODAY - UPDATES REQUIRED REGULARLY #
 # TODO: pull these in automatically
-nxm_supply_now = 6_779_309
-act_cover_now = 149_632
-cap_pool_now = 154_067
+nxm_supply_now = 6_775_867
+act_cover_now = 134_225
+cap_pool_now = 151_434
 
 # coingecko price api
 price_url = 'https://api.coingecko.com/api/v3/simple/price'
